0ceabec37060fefe93cc50086760c4f23210f9a9
Reynaldo Estrella <reynaldo@Reynaldos-MBP.lan>
2023-05-20 02:29:35 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,22 @@ import logo from './Pokédex_logo.png';
 
 function App() {
   return (
-    <div class="title">
-      <div class="titleleft">
-      <img src={logo} className="App-logo" alt="logo" />
-      </div>
-    </div>
+    // <div class="title">
+    //   <div class="titleleft">
+    //   <img src={logo} className="app-logo" alt="logo" />
+    //   <div className="buttons">
+    //     <button type="button">Home</button>
+    //     <button type="button">My List</button>
+    //   </div>
+    //   </div>
+    // </div>
+    <nav>
+      <ul class="navbar">
+        <li><a href="#">My List</a></li>
+        <li><a href="">Home</a></li>
+        <img src={logo} className="app-logo" alt="logo" />
+      </ul>
+    </nav>
   );
 }
 
